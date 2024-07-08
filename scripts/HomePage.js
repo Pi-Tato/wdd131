ddf35f0ecd843b3a6295893655c35ebd8dd1eadf
EnTech314 <rcmind64@gmail.com>
2024-07-08 13:20:15 -0600

    Moved files to my laptop.

--- scripts/HomePage.js
+++ scripts/HomePage.js
@@ -10,11 +10,11 @@ const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#000";
+		main.style.background = "#818174";
 		main.style.color = "#fff";
 		modeButton.textContent = "🔆";
 	} else {
-		main.style.background = "#eee";
+		main.style.background = "#f5f5dc";
 		main.style.color = "#000";
 		modeButton.textContent = "🕶️";
 	}
